bc9e9907d8c1a5863d74bd92c50435af3a544a6c
Hubert Pastyrzak <hubertpastyrzak@gmail.com>
2024-06-27 00:06:44 +0200

    user32: implement TRACKMOUSEEVENT structure

--- libraries/user32/winuser.h
+++ libraries/user32/winuser.h
@@ -2,6 +2,12 @@
 
 #include <windef.h>
 
+#define TME_CANCEL    0x80000000
+#define TME_HOVER     0x00000001
+#define TME_LEAVE     0x00000002
+#define TME_NONCLIENT 0x00000010
+#define TME_QUERY     0x40000000
+
 typedef void (*TIMERPROC)(HWND, UINT, UINT_PTR, DWORD);
 typedef LRESULT (*WNDPROC)(HWND, UINT, WPARAM, LPARAM);
 
@@ -37,6 +43,13 @@ struct WNDCLASSW {
   LPCWSTR lpszClassName;
 };
 
+struct TRACKMOUSEEVENT {
+  DWORD cbSize;
+  DWORD dwFlags;
+  HWND hwndTrack;
+  DWORD dwHoverTime;
+};
+
 typedef PAINTSTRUCT* PPAINTSTRUCT;
 typedef PAINTSTRUCT* NPPAINTSTRUCT;
 typedef PAINTSTRUCT* LPPAINTSTRUCT;
@@ -48,3 +61,5 @@ typedef MSG* LPMSG;
 typedef WNDCLASSW* PWNDCLASSW;
 typedef WNDCLASSW* NPWNDCLASSW;
 typedef WNDCLASSW* LPWNDCLASSW;
+
+typedef TRACKMOUSEEVENT* LPTRACKMOUSEEVENT;
