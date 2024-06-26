8528112b7a35b1561a57ca9af5644db2c7e30b5a
Hubert Pastyrzak <hubertpastyrzak@gmail.com>
2024-06-27 00:12:07 +0200

    comctl32: implement INITCOMMONCONTROLSEX structure

--- libraries/comctl32/commctrl.h
+++ libraries/comctl32/commctrl.h
@@ -1,5 +1,14 @@
 #pragma once
 
+#include <windef.h>
+
+struct INITCOMMONCONTROLSEX {
+  DWORD dwSize;
+  DWORD dwICC;
+};
+
+typedef INITCOMMONCONTROLSEX* LPINITCOMMONCONTROLSEX;
+
 extern "C" {
   BOOL _TrackMouseEvent(LPTRACKMOUSEEVENT lpEventTrack);
 }
