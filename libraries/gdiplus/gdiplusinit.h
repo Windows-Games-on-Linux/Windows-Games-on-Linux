28c0553dff23cae7f81b63b6091e2ee64cebb06d
Hubert Pastyrzak <hubertpastyrzak@gmail.com>
2024-06-27 00:36:33 +0200

    gdiplus: implement GdiplusStartupInput structure

--- libraries/gdiplus/gdiplusinit.h
+++ libraries/gdiplus/gdiplusinit.h
@@ -11,3 +11,12 @@ enum DebugEventLevel {
 typedef void (*DebugEventProc)(DebugEventLevel level, CHAR* message);
 typedef Status (*NotificationHookProc)(ULONG_PTR* token);
 typedef VOID (*NotificationUnhookProc)(ULONG_PTR token);
+
+struct GdiplusStartupInput {
+  UINT32 GdiplusVersion;
+  DebugEventProc DebugEventCallback;
+  BOOL SuppressBackgroundThread;
+  BOOL SuppressExternalCodecs;
+
+  GdiplusStartupInput(DebugEventProc debugEventCallback, BOOL suppressBackgroundThread, BOOL suppressExternalCodecs);
+};
