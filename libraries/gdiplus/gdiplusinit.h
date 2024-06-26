c220cdd7fc01bea7694dbefe8fca9908cc311bdd
Hubert Pastyrzak <hubertpastyrzak@gmail.com>
2024-06-27 00:26:37 +0200

    gdiplus: implement DebugEventProc type

--- libraries/gdiplus/gdiplusinit.h
+++ libraries/gdiplus/gdiplusinit.h
@@ -1,6 +1,10 @@
 #pragma once
 
+#include <windef.h>
+
 enum DebugEventLevel {
   DebugEventLevelFatal,
   DebugEventLevelWarning
 };
+
+typedef void (*DebugEventProc)(DebugEventLevel level, CHAR* message);
