93c564c5f3948d4b144824da1f89c03edef64840
Hubert Pastyrzak <hubertpastyrzak@gmail.com>
2024-06-29 17:59:33 +0200

    kernel32: implement PTOP_LEVEL_EXCEPTION_FILTER type

---
+++ libraries/kernel32/errhandlingapi.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <windef.h>
+#include "_winnt.h"
+
+typedef LONG (*PTOP_LEVEL_EXCEPTION_FILTER)(EXCEPTION_POINTERS* ExceptionInfo);
+typedef PTOP_LEVEL_EXCEPTION_FILTER LPTOP_LEVEL_EXCEPTION_FILTER;
