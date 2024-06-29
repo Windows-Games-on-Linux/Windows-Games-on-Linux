6dfba0b893c366ed62d743f368f49da0e70f8813
Hubert Pastyrzak <hubertpastyrzak@gmail.com>
2024-06-29 22:25:00 +0200

    kernel32: implement QueryPerformanceCounter function

---
+++ libraries/kernel32/profileapi.cpp
@@ -0,0 +1,12 @@
+#include "profileapi.h"
+
+#include <time.h>
+
+BOOL QueryPerformanceCounter(LARGE_INTEGER* lpPerformanceCount) {
+  struct timespec time;
+  clock_gettime(CLOCK_REALTIME, &time);
+
+  lpPerformanceCount->QuadPart = (time.tv_sec * 1000000000ULL) + time.tv_nsec;
+
+  return 1;
+}
