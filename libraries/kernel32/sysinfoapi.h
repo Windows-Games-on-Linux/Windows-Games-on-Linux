0b97aa2fe24c35bae01068e8833ce6d150ca2cdc
Hubert Pastyrzak <hubertpastyrzak@gmail.com>
2024-06-29 21:09:48 +0200

    kernel32: implement MEMORYSTATUSEX structure

---
+++ libraries/kernel32/sysinfoapi.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <windef.h>
+
+struct MEMORYSTATUSEX {
+  DWORD dwLength;
+  DWORD dwMemoryLoad;
+  DWORDLONG ullTotalPhys;
+  DWORDLONG ullAvailPhys;
+  DWORDLONG ullTotalPageFile;
+  DWORDLONG ullAvailPageFile;
+  DWORDLONG ullTotalVirtual;
+  DWORDLONG ullAvailVirtual;
+  DWORDLONG ullAvailExtendedVirtual;
+};
+
+typedef MEMORYSTATUSEX* LPMEMORYSTATUSEX;
