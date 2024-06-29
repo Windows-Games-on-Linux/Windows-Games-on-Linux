9806ed4d9728e5a59dd2df5fca2c5921df67b837
Hubert Pastyrzak <hubertpastyrzak@gmail.com>
2024-06-29 17:46:22 +0200

    common-headers: implement XSAVE_FORMAT structure

--- libraries/common-headers/ketypes.h
+++ libraries/common-headers/ketypes.h
@@ -8,3 +8,26 @@ struct M128A {
 };
 
 typedef M128A* PM128A;
+
+struct XSAVE_FORMAT {
+  USHORT ControlWord;
+  USHORT StatusWord;
+  UCHAR TagWord;
+  UCHAR Reserved1;
+  USHORT ErrorOpcode;
+  ULONG ErrorOffset;
+  USHORT ErrorSelector;
+  USHORT Reserved2;
+  ULONG DataOffset;
+  USHORT DataSelector;
+  USHORT Reserved3;
+  ULONG MxCsr;
+  ULONG MxCsr_Mask;
+  M128A FloatRegisters[8];
+  M128A XmmRegisters[8];
+  UCHAR Reserved4[192];
+  ULONG StackControl[7];
+  ULONG Cr0NpxState;
+};
+
+typedef XSAVE_FORMAT* PXSAVE_FORMAT;
