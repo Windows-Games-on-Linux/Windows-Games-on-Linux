1705990fb12089fea6b6764be189f2ee5deb2d17
Hubert Pastyrzak <hubertpastyrzak@gmail.com>
2024-06-29 18:08:14 +0200

    common-headers: implement WIN32_FIND_DATAW structure

--- libraries/common-headers/minwinbase.h
+++ libraries/common-headers/minwinbase.h
@@ -2,6 +2,18 @@
 
 //This file will be included by windef.h after all necessary types are defined
 
+#define IO_REPARSE_TAG_CSV         0x80000009
+#define IO_REPARSE_TAG_DEDUP       0x80000013
+#define IO_REPARSE_TAG_DFS         0x8000000A
+#define IO_REPARSE_TAG_DFSR        0x80000012
+#define IO_REPARSE_TAG_HSM         0xC0000004
+#define IO_REPARSE_TAG_HSM2        0x80000006
+#define IO_REPARSE_TAG_MOUNT_POINT 0xA0000003
+#define IO_REPARSE_TAG_NFS         0x80000014
+#define IO_REPARSE_TAG_SIS         0x80000007
+#define IO_REPARSE_TAG_SYMLINK     0xA000000C
+#define IO_REPARSE_TAG_WIM         0x80000008
+
 struct FILETIME {
   DWORD dwLodDateTime;
   DWORD dwHighDateTime;
@@ -18,8 +30,27 @@ struct SYSTEMTIME {
   WORD wMilliseconds;
 };
 
+struct WIN32_FIND_DATAW {
+  DWORD dwFileAttributes;
+  FILETIME ftCreationTime;
+  FILETIME ftLastAccessTime;
+  FILETIME ftLastWriteTime;
+  DWORD nFileSizeHigh;
+  DWORD nFileSizeLow;
+  DWORD dwReserved0;
+  DWORD dwReserved1;
+  CHAR cFileName[MAX_PATH];
+  CHAR cAlternateFileName[14];
+  DWORD dwFileType;
+  DWORD dwCreatorType;
+  WORD wFinderFlags;
+};
+
 typedef FILETIME* PFILETIME;
 typedef FILETIME* LPFILETIME;
 
 typedef SYSTEMTIME* PSYSTEMTIME;
 typedef SYSTEMTIME* LPSYSTEMTIME;
+
+typedef WIN32_FIND_DATAW* PWIN32_FIND_DATAW;
+typedef WIN32_FIND_DATAW* LPWIN32_FIND_DATAW;
