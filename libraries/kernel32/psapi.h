5d0134f3753e5596e0d80df0fe49eb55048e77dc
Hubert Pastyrzak <hubertpastyrzak@gmail.com>
2024-06-29 20:40:19 +0200

    kernel32: implement PROCESS_MEMORY_COUNTERS structure

---
+++ libraries/kernel32/psapi.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <windef.h>
+
+struct PROCESS_MEMORY_COUNTERS {
+  DWORD cb;
+  DWORD PageFaultCount;
+  SIZE_T PeakWorkingSetSize;
+  SIZE_T WorkingSetSize;
+  SIZE_T QuotaPeakPagedPoolUsage;
+  SIZE_T QuotaPagedPoolUsage;
+  SIZE_T QuotaPeakNonPagedPoolUsage;
+  SIZE_T QuotaNonPagedPoolUsage;
+  SIZE_T PagefileUsage;
+  SIZE_T PeakPagefileUsage;
+};
+
+typedef PROCESS_MEMORY_COUNTERS* PPROCESS_MEMORY_COUNTERS;
