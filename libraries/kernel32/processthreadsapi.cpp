c1bf7e97e469b8b5137fffc9bec62313f072f0e9
Hubert Pastyrzak <hubertpastyrzak@gmail.com>
2024-07-03 00:57:21 +0200

    kernel32: implement IsProcessorFeaturePresent function
    More specifically, this commit only implements the PF_XMMI64_INSTRUCTIONS_AVAILABLE feature check.

--- libraries/kernel32/processthreadsapi.cpp
+++ libraries/kernel32/processthreadsapi.cpp
@@ -2,6 +2,7 @@
 
 #include <pthread.h>
 #include <unistd.h>
+#include <iostream>
 
 DWORD GetCurrentThreadId() {
   return pthread_self();
@@ -9,4 +10,36 @@ DWORD GetCurrentThreadId() {
 
 DWORD GetCurrentProcessId() {
   return getpid();
+}
+
+BOOL IsProcessorFeaturePresent(DWORD ProcessorFeature) {
+  if (ProcessorFeature == PF_XMMI64_INSTRUCTIONS_AVAILABLE) {
+    //First, push all the registers that will be affected by CPUID
+    //We have to restore them later
+    asm ("push %eax");
+    asm ("push %ebx");
+    asm ("push %ecx");
+    asm ("push %edx");
+
+    //Query available processor features
+    asm ("mov $1, %eax");
+    asm ("cpuid");
+
+    //Bit 26 of EDX determines whether SSE2 is available
+    BOOL available;
+    asm ("and $0x04000000, %edx");
+    asm ("shr $26, %edx");
+    asm volatile ("movl %%edx, %0" : "=r" (available));
+
+    //Restore the registers we pushed previously
+    asm ("pop %edx");
+    asm ("pop %ecx");
+    asm ("pop %ebx");
+    asm ("pop %eax");
+
+    return available;
+  }
+
+  std::cout << "IsProcessorFeaturePresent: Unimplemented ProcessorFeature: " << ProcessorFeature << std::endl;
+  return 0;
 }
