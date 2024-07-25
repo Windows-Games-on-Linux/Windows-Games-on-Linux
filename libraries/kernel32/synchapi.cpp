c3046ffc9a512d176e26dfd60ebd651b0d9df9ad
Hubert Pastyrzak <hubertpastyrzak@gmail.com>
2024-07-25 19:08:22 +0100

    kernel32: implement EnterCriticalSection function

---
+++ libraries/kernel32/synchapi.cpp
@@ -0,0 +1,14 @@
+#include "synchapi.h"
+
+#include <pthread.h>
+
+void __attribute__((stdcall)) EnterCriticalSection(LPCRITICAL_SECTION lpCriticalSection) {
+  //TODO: Implement the EXCEPTION_POSSIBLE_DEADLOCK exception
+  //This exception requires a timeout specified in the following registry value:
+  //HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\SessionManager\CriticalSectionTimeout
+  //A properly written application shouldn't raise this exception, so for now let's skip this part
+  pthread_mutex_lock((pthread_mutex_t*)lpCriticalSection->LockSemaphore);
+
+  lpCriticalSection->LockCount++;
+}
+
