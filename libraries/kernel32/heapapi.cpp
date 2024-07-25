d0144aa09108729005671024cc7650e60a506dd5
Hubert Pastyrzak <hubertpastyrzak@gmail.com>
2024-07-25 18:52:18 +0100

    kernel32: implement HeapAlloc function

--- libraries/kernel32/heapapi.cpp
+++ libraries/kernel32/heapapi.cpp
@@ -1,8 +1,38 @@
 #include "heapapi.h"
 
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+
 HANDLE __attribute__((stdcall)) GetProcessHeap() {
   //Unlike Windows, Linux only has one global heap and doesn't support multiple heaps natively
   //Let's just return 1 as a handle for the default heap of a process
   return (HANDLE)1;
+}
+
+LPVOID __attribute__((stdcall)) HeapAlloc(HANDLE hHeap, DWORD dwFlags, SIZE_T dwBytes) {
+  //For now only the default heap is supported
+  if (hHeap != (HANDLE)1) {
+    return nullptr;
+  }
+
+  if (dwFlags & HEAP_GENERATE_EXCEPTIONS) {
+    std::cout << "HeapAlloc: Unimplemented Flag: HEAP_GENERATE_EXCEPTIONS" << std::endl;
+  }
+
+  if (dwFlags & HEAP_NO_SERIALIZE) {
+    std::cout << "HeapAlloc: Unimplemented Flag: HEAP_NO_SERIALIZE" << std::endl;
+  }
+
+  void* memory = malloc(dwBytes);
+  if (!memory) {
+    return nullptr;
+  }
+
+  if (dwFlags & HEAP_ZERO_MEMORY) {
+    memset(memory, 0, dwBytes);
+  }
+
+  return memory;
 }
 
--- libraries/kernel32/heapapi.h
+++ libraries/kernel32/heapapi.h
@@ -2,7 +2,12 @@
 
 #include <windef.h>
 
+#define HEAP_GENERATE_EXCEPTIONS 0x00000004
+#define HEAP_NO_SERIALIZE        0x00000001
+#define HEAP_ZERO_MEMORY         0x00000008
+
 extern "C" {
   HANDLE __attribute__((stdcall)) GetProcessHeap();
+  LPVOID __attribute__((stdcall)) HeapAlloc(HANDLE hHeap, DWORD dwFlags, SIZE_T dwBytes);
 };
 
