2879e168ec3240e1e04f7c3b58a9d5536e0f00a5
Hubert Pastyrzak <hubertpastyrzak@gmail.com>
2024-07-25 18:23:26 +0100

    kernel32: implement GetLastError

--- libraries/kernel32/errhandlingapi.h
+++ libraries/kernel32/errhandlingapi.h
@@ -5,3 +5,8 @@
 
 typedef LONG (*PTOP_LEVEL_EXCEPTION_FILTER)(EXCEPTION_POINTERS* ExceptionInfo);
 typedef PTOP_LEVEL_EXCEPTION_FILTER LPTOP_LEVEL_EXCEPTION_FILTER;
+
+extern "C" {
+  DWORD __attribute__((stdcall)) GetLastError();
+};
+
