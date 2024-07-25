75c43891a89d85c56eb010a34d2649a9cd2d2bd4
Hubert Pastyrzak <hubertpastyrzak@gmail.com>
2024-07-25 18:31:53 +0100

    api-ms-win-core-fibers-l1-1-0: implement FlsSetValue function

--- libraries/api-ms-win-core-fibers-l1-1-0/fibersapi.h
+++ libraries/api-ms-win-core-fibers-l1-1-0/fibersapi.h
@@ -5,5 +5,6 @@
 extern "C" {
   DWORD __attribute__((stdcall)) FlsAlloc(PFLS_CALLBACK_FUNCTION lpCallback);
   PVOID __attribute__((stdcall)) FlsGetValue(DWORD dwFlsIndex);
+  BOOL __attribute__((stdcall)) FlsSetValue(DWORD dwFlsIndex, PVOID lpFlsData);
 };
 
