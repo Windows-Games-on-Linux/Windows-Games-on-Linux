cd99a7a2a1d72fe3009b8d52974f3e14b00b0db2
Hubert Pastyrzak <hubertpastyrzak@gmail.com>
2024-07-25 19:41:42 +0100

    api-ms-win-core-synch-l1-2-0: make critical sections recursive
    Add support for recursion to the critical sections created by InitializeCriticalSectionEx

--- libraries/api-ms-win-core-synch-l1-2-0/synchapi.cpp
+++ libraries/api-ms-win-core-synch-l1-2-0/synchapi.cpp
@@ -3,8 +3,15 @@
 #include <pthread.h>
 
 BOOL __attribute__((stdcall)) InitializeCriticalSectionEx(LPCRITICAL_SECTION lpCriticalSection, DWORD dwSpinCount, DWORD Flags) {
+  pthread_mutexattr_t mutexattr;
+  pthread_mutexattr_init(&mutexattr);
+
+  pthread_mutexattr_settype(&mutexattr, PTHREAD_MUTEX_RECURSIVE);
+
   pthread_mutex_t* mutex = new pthread_mutex_t();
-  pthread_mutex_init(mutex, nullptr);
+  pthread_mutex_init(mutex, &mutexattr);
+
+  pthread_mutexattr_destroy(&mutexattr);
 
   //DebugInfo is supposed to be initialized when a debugger is attached to the process
   //For now let's just assume that it's not
