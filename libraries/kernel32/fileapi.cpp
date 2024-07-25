558b5c351306bea74d5b760c69e87a8471a3a760
Hubert Pastyrzak <hubertpastyrzak@gmail.com>
2024-07-25 23:51:34 +0100

    kernel32: implement GetFileType function

---
+++ libraries/kernel32/fileapi.cpp
@@ -0,0 +1,49 @@
+#include "fileapi.h"
+
+#include <sys/stat.h>
+#include <cstdio>
+
+DWORD __attribute__((stdcall)) GetFileType(HANDLE hFile) {
+  int fd = -1;
+
+  //For now, only the standard handles are supported
+
+  //STD_INPUT_HANDLE
+  if (hFile == (HANDLE)2) {
+    fd = fileno(stdin);
+  }
+
+  //STD_OUTPUT_HANDLE
+  if (hFile == (HANDLE)3) {
+    fd = fileno(stdout);
+  }
+
+  //STD_ERROR_HANDLE
+  if (hFile == (HANDLE)4) {
+    fd = fileno(stderr);
+  }
+
+  if (fd == -1) {
+    return FILE_TYPE_UNKNOWN;
+  }
+
+  struct stat fileInfo;
+  fstat(fd, &fileInfo);
+
+  if (S_ISCHR(fileInfo.st_mode)) {
+    return FILE_TYPE_CHAR;
+  }
+
+  if (S_ISREG(fileInfo.st_mode)) {
+    return FILE_TYPE_DISK;
+  }
+
+  if (S_ISFIFO(fileInfo.st_mode)) {
+    return FILE_TYPE_PIPE;
+  }
+
+  //TODO: Implement support for FILE_TYPE_REMOTE
+
+  return FILE_TYPE_UNKNOWN;
+}
+
--- libraries/kernel32/fileapi.h
+++ libraries/kernel32/fileapi.h
@@ -2,6 +2,12 @@
 
 #include <windef.h>
 
+#define FILE_TYPE_CHAR    0x0002
+#define FILE_TYPE_DISK    0x0001
+#define FILE_TYPE_PIPE    0x0003
+#define FILE_TYPE_REMOTE  0x8000
+#define FILE_TYPE_UNKNOWN 0x0000
+
 struct BY_HANDLE_FILE_INFORMATION {
   DWORD dwFileAttributes;
   FILETIME ftCreationTime;
@@ -17,3 +23,8 @@ struct BY_HANDLE_FILE_INFORMATION {
 
 typedef BY_HANDLE_FILE_INFORMATION* PBY_HANDLE_FILE_INFORMATION;
 typedef BY_HANDLE_FILE_INFORMATION* LPBY_HANDLE_FILE_INFORMATION;
+
+extern "C" {
+  DWORD __attribute__((stdcall)) GetFileType(HANDLE hFile);
+};
+
