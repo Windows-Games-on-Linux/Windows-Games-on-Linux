8bddd06eb0c0c583adf85378374b7e1572ba332a
Hubert Pastyrzak <hubertpastyrzak@gmail.com>
2024-07-25 23:29:56 +0100

    kernel32: implement GetStdHandle function

---
+++ libraries/kernel32/processenv.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <windef.h>
+
+#define STD_INPUT_HANDLE  ((DWORD)-10)
+#define STD_OUTPUT_HANDLE ((DWORD)-11)
+#define STD_ERROR_HANDLE  ((DWORD)-12)
+
+extern "C" {
+  HANDLE __attribute__((stdcall)) GetStdHandle(DWORD nStdHandle);
+};
+
