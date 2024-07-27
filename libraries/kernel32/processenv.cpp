997c383b971673844a1cfffbcd1e600eb111d039
Hubert Pastyrzak <hubertpastyrzak@gmail.com>
2024-07-27 13:41:31 +0100

    kernel32: implement GetCommandLineA function

--- libraries/kernel32/processenv.cpp
+++ libraries/kernel32/processenv.cpp
@@ -1,5 +1,11 @@
 #include "processenv.h"
 
+#include <cstdio>
+#include <cstring>
+
+//TODO: This needs to be deallocated when the application exits
+LPSTR commandLineA = nullptr;
+
 HANDLE __attribute__((stdcall)) GetStdHandle(DWORD nStdHandle) {
   //Let's assume the following handle values:
   // - STD_INPUT_HANDLE: 2
@@ -19,5 +25,145 @@ HANDLE __attribute__((stdcall)) GetStdHandle(DWORD nStdHandle) {
   }
 
   return nullptr;
+}
+
+LPSTR __attribute__((stdcall)) GetCommandLineA() {
+  //If the command line buffer is already available, simply return it
+  if (commandLineA) {
+    return commandLineA;
+  }
+
+  //If not, we need to make it
+  //"/proc/self/cmdline" contains a list of arguments separated by NULL characters, like this
+  //We need to convert it into a list of quoted arguments separated by space characters
+  FILE* cmdline = fopen("/proc/self/cmdline", "r");
+
+  //First, let's calculate the length of the output command line buffer
+  size_t length = 0;
+  {
+    char buffer[1024];
+
+    //Are we currently inside of an argument?
+    bool insideArgument = true; //The very first character in the sequence is part of the first argument
+
+    //We need to know which argument we're currently in
+    //The first argument (argumentIndex = 0) needs to be skipped, because it's the path of the 'Windows Games on Linux' executable
+    int argumentIndex = 0;
+
+    while (fgets(buffer, 1024, cmdline)) {
+      for (int i = 0; i < 1024; i++) {
+        //Handle a NULL character
+        if (buffer[i] == 0) {
+          //If we're already outside of an argument (meaning that there was another NULL character before this one),
+          //this means that it's the end of the sequence
+          if (!insideArgument) {
+            break;
+          }
+
+          //Quit the argument
+          insideArgument = false;
+
+          //Allocate a byte for a quote character that ends the argument
+          //Note: First argument is skipped, see above
+          if (argumentIndex > 0) {
+            length++;
+          }
+
+          argumentIndex++;
+          continue;
+        }
+
+        //Handle all the other characters
+        //Note: First argument is skipped, see above
+        if (argumentIndex > 0) {
+          //If we're currently outside of an argument, that means that we're entering a new argument
+          if (!insideArgument) {
+            insideArgument = true;
+
+            //If this is the first argument that will be put into the command line buffer,
+            //simply allocate a byte for a quote character
+            if (argumentIndex == 1) {
+              length++;
+            } else {
+              //If it's any other argument, allocate 2 bytes: one for a separating space character, and one for the opening quote
+              length += 2;
+            }
+          }
+
+          length++;
+        }
+      }
+    }
+  }
+
+  //Now, allocate a command line buffer and write to it using an algorithm similar to the above one
+  commandLineA = new char[length + 1];
+
+  //The current position in the command line buffer
+  int j = 0;
+
+  {
+    fseek(cmdline, 0, SEEK_SET);
+    char buffer[1024];
+
+    //Are we currently inside of an argument?
+    bool insideArgument = true; //The very first character in the sequence is part of the first argument
+
+    //We need to know which argument we're currently in
+    //The first argument (argumentIndex = 0) needs to be skipped, because it's the path of the 'Windows Games on Linux' executable
+    int argumentIndex = 0;
+
+    while (fgets(buffer, 1024, cmdline)) {
+      for (int i = 0; i < 1024; i++) {
+        //Handle a NULL character
+        if (buffer[i] == 0) {
+          //If we're already outside of an argument (meaning that there was another NULL character before this one),
+          //this means that it's the end of the sequence
+          if (!insideArgument) {
+            break;
+          }
+
+          //Quit the argument
+          insideArgument = false;
+
+          //End the argument with a quote character
+          //Note: First argument is skipped, see above
+          if (argumentIndex > 0) {
+            commandLineA[j++] = '"';
+          }
+
+          argumentIndex++;
+          continue;
+        }
+
+        //Handle all the other characters
+        //Note: First argument is skipped, see above
+        if (argumentIndex > 0) {
+          //If we're currently outside of an argument, that means that we're entering a new argument
+          if (!insideArgument) {
+            insideArgument = true;
+
+            //If this is the first argument that will be put into the command line buffer,
+            //simply start it with a quote character
+            if (argumentIndex == 1) {
+              commandLineA[j++] = '"';
+            } else {
+              //If it's any other argument, add a separating space character and start the argument with a quote character
+              commandLineA[j++] = ' ';
+              commandLineA[j++] = '"';
+            }
+          }
+
+          commandLineA[j++] = buffer[i];
+        }
+      }
+    }
+  }
+
+  //Finally, end the command line with a NULL character
+  commandLineA[j++] = 0;
+
+  fclose(cmdline);
+  return commandLineA;
 }
 
