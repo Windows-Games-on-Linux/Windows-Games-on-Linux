8ebc43fcb3ab6590ae3770c146503eab99585101
Hubert Pastyrzak <hubertpastyrzak@gmail.com>
2024-07-28 19:39:49 +0100

    kernel32: implement WideCharToMultiByte function

--- libraries/kernel32/stringapiset.cpp
+++ libraries/kernel32/stringapiset.cpp
@@ -155,3 +155,134 @@ BOOL __attribute__((stdcall)) GetStringTypeW(DWORD dwInfoType, LPCWCH lpSrcStr, 
   return 0;
 }
 
+int __attribute__((stdcall)) WideCharToMultiByte(UINT CodePage, DWORD dwFlags, LPCWCH lpWideCharStr, int cchWideChar, LPSTR lpMultiByteStr, int cbMultiByte, LPCCH lpDefaultChar, LPBOOL lpUsedDefaultChar) {
+  if (dwFlags & WC_COMPOSITECHECK) {
+    std::cout << "WideCharToMultiByte: Unimplemented Flag: WC_COMPOSITECHECK" << std::endl;
+  }
+
+  if (dwFlags & WC_DEFAULTCHAR) {
+    std::cout << "WideCharToMultiByte: Unimplemented Flag: WC_DEFAULTCHAR" << std::endl;
+  }
+
+  if (dwFlags & WC_DISCARDNS) {
+    std::cout << "WideCharToMultiByte: Unimplemented Flag: WC_DISCARDNS" << std::endl;
+  }
+
+  if (dwFlags & WC_SEPCHARS) {
+    std::cout << "WideCharToMultiByte: Unimplemented Flag: WC_SEPCHARS" << std::endl;
+  }
+
+  if (dwFlags & WC_ERR_INVALID_CHARS) {
+    std::cout << "WideCharToMultiByte: Unimplemented Flag: WC_ERR_INVALID_CHARS" << std::endl;
+  }
+
+  if (dwFlags & WC_NO_BEST_FIT_CHARS) {
+    std::cout << "WideCharToMultiByte: Unimplemented Flag: WC_NO_BEST_FIT_CHARS" << std::endl;
+  }
+
+  if (cchWideChar == 0) {
+    return 0;
+  }
+
+  if (lpUsedDefaultChar) {
+    *lpUsedDefaultChar = 0;
+  }
+
+  if (CodePage == 1252) {
+    //If cbMultiByte = 0, calculate and return the required output buffer size
+    if (cbMultiByte == 0) {
+      //If cchWideChar != -1, it specifies the size of the input buffer
+      if (cchWideChar != -1) {
+        if (lpUsedDefaultChar) {
+          //Scan for characters that cannot be represented anyway if the application expects this info
+          int i = 0;
+          for (const char16_t* c = lpWideCharStr; ; c++) {
+            if (*c >= 0x100) {
+              *lpUsedDefaultChar = 1;
+              break;
+            }
+
+            i++;
+            if (i == cchWideChar) {
+              break;
+            }
+          }
+        }
+
+        return cchWideChar;
+      }
+
+      //Otherwise, the size needs to be calculated, including the NULL character
+      int size = 0;
+      for (const char16_t* c = lpWideCharStr; ; c++) {
+        if (lpUsedDefaultChar && *c >= 0x100) {
+          *lpUsedDefaultChar = 1;
+        }
+
+        size++;
+        if (*c == 0) {
+          break;
+        }
+      }
+
+      return size;
+    }
+
+    //If cbMultiByte != 0, perform the actual conversion
+
+    //If cchWideChar = -1, copy until the end of the output buffer or until the NULL character
+    if (cchWideChar == -1) {
+      int j = 0;
+      for (const char16_t* c = lpWideCharStr; ; c++) {
+        if (*c <= 0xFF) {
+          lpMultiByteStr[j++] = *c;
+        } else {
+          if (lpDefaultChar) {
+            lpMultiByteStr[j++] = *lpDefaultChar;
+          } else {
+            lpMultiByteStr[j++] = '?';
+          }
+
+          if (lpUsedDefaultChar) {
+            *lpUsedDefaultChar = 1;
+          }
+        }
+
+        if (*c == 0 || j == cbMultiByte) {
+          break;
+        }
+      }
+
+      return j;
+    }
+
+    //Otherwise, copy as much as possible
+    int size = std::min(cchWideChar, cbMultiByte);
+    int j = 0;
+    for (const char16_t* c = lpWideCharStr; ; c++) {
+      if (*c <= 0xFF) {
+        lpMultiByteStr[j++] = *c;
+      } else {
+        if (lpDefaultChar) {
+          lpMultiByteStr[j++] = *lpDefaultChar;
+        } else {
+          lpMultiByteStr[j++] = '?';
+        }
+
+        if (lpUsedDefaultChar) {
+          *lpUsedDefaultChar = 1;
+        }
+      }
+
+      if (j == size) {
+        break;
+      }
+    }
+
+    return size;
+  }
+
+  std::cout << "WideCharToMultiByte: Unimplemented Code Page Identifier: " << CodePage << std::endl;
+  return 0;
+}
+
