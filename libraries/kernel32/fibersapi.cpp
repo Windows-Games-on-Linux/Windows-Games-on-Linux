e233671a206fe0f6637c0d0453105e4bf86870c9
Hubert Pastyrzak <hubertpastyrzak@gmail.com>
2024-08-02 16:29:55 +0100

    kernel32: implement FlsAlloc function

---
+++ libraries/kernel32/fibersapi.cpp
@@ -0,0 +1,11 @@
+#include "fibersapi.h"
+
+#include <pthread.h>
+
+DWORD WINAPI FlsAlloc(PFLS_CALLBACK_FUNCTION lpCallback) {
+  pthread_key_t key;
+  pthread_key_create(&key, lpCallback);
+
+  return key;
+}
+
