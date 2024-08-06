2c6d35294cf7f62d4ac7c213f7314c2f7e2a8447
Hubert Pastyrzak <hubertpastyrzak@gmail.com>
2024-08-06 22:46:42 +0100

    kernel32: implement SetUnhandledExceptionFilter function and signal handling

--- libraries/kernel32/errhandlingapi.h
+++ libraries/kernel32/errhandlingapi.h
@@ -3,11 +3,12 @@
 #include <windef.h>
 #include "_winnt.h"
 
-typedef LONG (*PTOP_LEVEL_EXCEPTION_FILTER)(EXCEPTION_POINTERS* ExceptionInfo);
+typedef LONG (*PTOP_LEVEL_EXCEPTION_FILTER)(EXCEPTION_POINTERS* ExceptionInfo) WINAPI;
 typedef PTOP_LEVEL_EXCEPTION_FILTER LPTOP_LEVEL_EXCEPTION_FILTER;
 
 extern "C" {
   DWORD WINAPI GetLastError();
   void WINAPI SetLastError(DWORD dwErrorCode);
+  LPTOP_LEVEL_EXCEPTION_FILTER WINAPI SetUnhandledExceptionFilter(LPTOP_LEVEL_EXCEPTION_FILTER lpTopLevelExceptionFilter);
 };
 
