8702b77f4ae33b54325ba2aab093b282ebbe16b3
Hubert Pastyrzak <hubertpastyrzak@gmail.com>
2024-08-06 22:23:48 +0100

    kernel32: implement RtlLookupFunctionEntry function

--- libraries/kernel32/_winnt.h
+++ libraries/kernel32/_winnt.h
@@ -4,5 +4,6 @@
 
 extern "C" {
   VOID WINAPI RtlCaptureContext(PCONTEXT ContextRecord);
+  PRUNTIME_FUNCTION WINAPI RtlLookupFunctionEntry(DWORD64 ControlPc, PDWORD64 ImageBase, PUNWIND_HISTORY_TABLE HistoryTable);
 };
 
