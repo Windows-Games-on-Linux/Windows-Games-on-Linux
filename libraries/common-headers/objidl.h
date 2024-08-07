200a90b7cf6c2f8868483b2aa911ef1520e3f1d4
Hubert Pastyrzak <hubertpastyrzak@gmail.com>
2024-08-07 14:26:16 +0100

    common-headers: implement STATSTG structure

--- libraries/common-headers/objidl.h
+++ libraries/common-headers/objidl.h
@@ -4,3 +4,17 @@
 
 typedef WCHAR* LPOLESTR;
 
+struct STATSTG {
+  LPOLESTR pwcsName;
+  DWORD type;
+  ULARGE_INTEGER cbSize;
+  FILETIME mtime;
+  FILETIME ctime;
+  FILETIME atime;
+  DWORD grfMode;
+  DWORD grfLocksSupported;
+  CLSID clsid;
+  DWORD grfStateBits;
+  DWORD reserved;
+};
+
