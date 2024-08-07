d81fef8a92303da6403e4a8d086b803e6743bf6f
Hubert Pastyrzak <hubertpastyrzak@gmail.com>
2024-08-07 14:55:27 +0100

    common-headers: add common HRESULT values

---
+++ libraries/common-headers/winerror.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#define S_OK           0x00000000
+#define E_AGORT        0x80004004
+#define E_ACCESSDENIED 0x80070005
+#define E_FAIL         0x80004005
+#define E_HANDLE       0x80070006
+#define E_INVALIDARG   0x80070057
+#define E_NOINTERFACE  0x80004002
+#define E_NOTIMPL      0x80004001
+#define E_OUTOFMEMORY  0x8007000E
+#define E_POINTER      0x80004003
+#define E_UNEXPECTED   0x8000FFFF
+
