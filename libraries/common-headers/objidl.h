994b2136ad9a33d9d977bdcd86863902cb0f415b
Hubert Pastyrzak <hubertpastyrzak@gmail.com>
2024-08-07 14:28:05 +0100

    common-headers: implement IStream interface

--- libraries/common-headers/objidl.h
+++ libraries/common-headers/objidl.h
@@ -24,3 +24,13 @@ class ISequentialStream : public IUnknown {
   virtual HRESULT WINAPI Write(const void* pv, ULONG cb, ULONG* pcbWritten) = 0;
 };
 
+class IStream : public ISequentialStream {
+  virtual HRESULT WINAPI Seek(LARGE_INTEGER dlibMove, DWORD dwOrigin, ULARGE_INTEGER* plibNewPosition) = 0;
+  virtual HRESULT WINAPI SetSize(ULARGE_INTEGER libNewSize) = 0;
+  virtual HRESULT WINAPI Commit(DWORD grfCommitFlags) = 0;
+  virtual HRESULT WINAPI Revert() = 0;
+  virtual HRESULT WINAPI CopyTo(IStream* pstm, ULARGE_INTEGER cb, ULARGE_INTEGER* pcbRead, ULARGE_INTEGER* pcbWritten) = 0;
+  virtual HRESULT WINAPI Stat(STATSTG* pstatstg, DWORD grfStatFlag) = 0;
+  virtual HRESULT WINAPI Clone(IStream** ppstm) = 0;
+};
+
