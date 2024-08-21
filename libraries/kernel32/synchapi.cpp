bd71c464b1583a4d14457c266a58e127b2fdb177
Hubert Pastyrzak <hubertpastyrzak@gmail.com>
2024-08-21 23:13:57 +0100

    synchapi.h: implement InitializeCriticalSectionEx function

--- libraries/kernel32/synchapi.cpp
+++ libraries/kernel32/synchapi.cpp
@@ -62,3 +62,27 @@ HANDLE WINAPI CreateEventW(LPSECURITY_ATTRIBUTES lpEventAttributes, BOOL bManual
   return event;
 }
 
+BOOL WINAPI InitializeCriticalSectionEx(LPCRITICAL_SECTION lpCriticalSection, DWORD dwSpinCount, DWORD Flags) {
+  pthread_mutexattr_t mutexattr;
+  pthread_mutexattr_init(&mutexattr);
+
+  pthread_mutexattr_settype(&mutexattr, PTHREAD_MUTEX_RECURSIVE);
+
+  pthread_mutex_t* mutex = new pthread_mutex_t();
+  pthread_mutex_init(mutex, &mutexattr);
+
+  pthread_mutexattr_destroy(&mutexattr);
+
+  //DebugInfo is supposed to be initialized when a debugger is attached to the process
+  //For now let's just assume that it's not
+  lpCriticalSection->DebugInfo = nullptr;
+
+  lpCriticalSection->LockCount = 0;
+  lpCriticalSection->RecursionCount = 0;
+  lpCriticalSection->OwningThread = nullptr;
+  lpCriticalSection->LockSemaphore = (PVOID)mutex;
+  lpCriticalSection->SpinCount = dwSpinCount;
+
+  return 1;
+}
+
