d36f095bb06a8d3c2f4f3ea528cd299a3bc5d909
Hubert Pastyrzak <hubertpastyrzak@gmail.com>
2024-08-22 10:41:19 +0100

    winmm: implement timeGetDevCaps function

---
+++ libraries/winmm/timeapi.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <windef.h>
+#include "mmsystem.h"
+
+#define TIMERR_BASE    96
+#define TIMERR_NOCANDO (TIMERR_BASE + 1)
+
+extern "C" {
+  MMRESULT WINAPI timeGetDevCaps(LPTIMECAPS ptc, UINT cbtc);
+};
+
