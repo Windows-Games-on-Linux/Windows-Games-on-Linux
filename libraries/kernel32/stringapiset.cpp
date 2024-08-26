8905c77969f81084fe07667786d9e71fc0afa5ce
Hubert Pastyrzak <hubertpastyrzak@gmail.com>
2024-08-26 13:46:48 +0100

    kernel32: WideCharToMultiByte: implement support for UTF-8 codepage

--- libraries/kernel32/stringapiset.cpp
+++ libraries/kernel32/stringapiset.cpp
@@ -172,10 +172,6 @@ int WINAPI WideCharToMultiByte(UINT CodePage, DWORD dwFlags, LPCWCH lpWideCharSt
     std::cout << "WideCharToMultiByte: Unimplemented Flag: WC_SEPCHARS" << std::endl;
   }
 
-  if (dwFlags & WC_ERR_INVALID_CHARS) {
-    std::cout << "WideCharToMultiByte: Unimplemented Flag: WC_ERR_INVALID_CHARS" << std::endl;
-  }
-
   if (dwFlags & WC_NO_BEST_FIT_CHARS) {
     std::cout << "WideCharToMultiByte: Unimplemented Flag: WC_NO_BEST_FIT_CHARS" << std::endl;
   }
@@ -280,6 +276,122 @@ int WINAPI WideCharToMultiByte(UINT CodePage, DWORD dwFlags, LPCWCH lpWideCharSt
     }
 
     return size;
+  }
+
+  if (CodePage == 65001) {
+    //If cbMultiByte = 0, calculate and return the required output buffer size
+    if (cbMultiByte == 0) {
+      //If cchWideChar != -1, it specifies the size of the input buffer
+      if (cchWideChar != -1) {
+        int i = 0;
+        int size = 0;
+        for (const char16_t* c = lpWideCharStr; ; c++) {
+          if (*c <= 0x007F) {
+            size++;
+          } else if (*c >= 0x0080 && *c <= 0x07FF) {
+            size += 2;
+          } else if (*c >= 0x0800) {
+            size += 3;
+          }
+
+          i++;
+          if (i == cchWideChar) {
+            break;
+          }
+        }
+
+        return size;
+      }
+
+      //Otherwise, the size needs to be calculated, including the NULL character
+      int size = 0;
+      for (const char16_t* c = lpWideCharStr; ; c++) {
+        if (*c <= 0x007F) {
+          size++;
+        } else if (*c >= 0x0080 && *c <= 0x07FF) {
+          size += 2;
+        } else if (*c >= 0x0800) {
+          size += 3;
+        }
+
+        if (*c == 0) {
+          break;
+        }
+      }
+
+      return size;
+    }
+
+    //If cbMultiByte != 0, perform the actual conversion
+
+    //If cchWideChar = -1, copy until the end of the output buffer or until the NULL character
+    if (cchWideChar == -1) {
+      int j = 0;
+      for (const char16_t* c = lpWideCharStr; ; c++) {
+        if (*c <= 0x007F) {
+          lpMultiByteStr[j++] = *c;
+        } else if (*c >= 0x0080 && *c <= 0x07FF) {
+          lpMultiByteStr[j++] = 0xC0 | ((*c >> 6) & 0x1F);
+          if (j == cbMultiByte) {
+            break;
+          }
+
+          lpMultiByteStr[j++] = 0x80 | (*c & 0x3F);
+        } else if (*c >= 0x0800) {
+          lpMultiByteStr[j++] = 0xE0 | ((*c >> 12) & 0x0F);
+          if (j == cbMultiByte) {
+            break;
+          }
+
+          lpMultiByteStr[j++] = 0x80 | ((*c >> 6) & 0x3F);
+          if (j == cbMultiByte) {
+            break;
+          }
+
+          lpMultiByteStr[j++] = 0x80 | (*c & 0x3F);
+        }
+
+        if (*c == 0 || j == cbMultiByte) {
+          break;
+        }
+      }
+
+      return j;
+    }
+
+    //Otherwise, copy as much as possible
+    int i = 0; //lpWideCharStr position
+    int j = 0; //lpMultiByteStr position
+    for (const char16_t* c = lpWideCharStr; ; c++) {
+      if (*c <= 0x007F) {
+        lpMultiByteStr[j++] = *c;
+      } else if (*c >= 0x0080 && *c <= 0x07FF) {
+        lpMultiByteStr[j++] = 0xC0 | ((*c >> 6) & 0x1F);
+        if (j == cbMultiByte) {
+          break;
+        }
+
+        lpMultiByteStr[j++] = 0x80 | (*c & 0x3F);
+      } else if (*c >= 0x0800) {
+        lpMultiByteStr[j++] = 0xE0 | ((*c >> 12) & 0x0F);
+        if (j == cbMultiByte) {
+          break;
+        }
+
+        lpMultiByteStr[j++] = 0x80 | ((*c >> 6) & 0x3F);
+        if (j == cbMultiByte) {
+          break;
+        }
+
+        lpMultiByteStr[j++] = 0x80 | (*c & 0x3F);
+      }
+
+      if (i == cchWideChar || j == cbMultiByte) {
+        break;
+      }
+    }
+
+    return j;
   }
 
   std::cout << "WideCharToMultiByte: Unimplemented Code Page Identifier: " << CodePage << std::endl;
